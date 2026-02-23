a9eb7e759fc456f24322faafdcc761eaf14f6cc8
Pubudu Perera <pubupere32@gmail.com>
2026-02-23 20:19:57 +0530

    Initial Commit

---
+++ iOS_CW_242P/iOS_CW_242PApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CW_242PApp.swift
+//  iOS_CW_242P
+//
+//  Created by Pubudu Perera on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CW_242PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
