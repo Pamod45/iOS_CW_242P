e7696ef03942d4287a0fb0b04181c5695975cf7b
Sarith Ranathunge <sarithperera14@gmail.com>
2026-03-09 01:12:03 +0530

    Make the main structure in phamasist UI

--- iOS_CW_242P/Views/PatientScreens/PatientAppContainer.swift
+++ iOS_CW_242P/Views/PatientScreens/PatientAppContainer.swift
@@ -11,32 +11,42 @@ import SwiftUI
 struct PatientAppContainer: View {
     @EnvironmentObject var authViewModel: AuthViewModel
     @State private var selectedTab = 0
+    @State private var editRole: UserRole        = .patient
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            DashboardView()
-                .tabItem {
-                    Label("Home", systemImage: "house.fill")
-                }
-                .tag(0)
-            
-            CheckInView()
-                .tabItem {
-                    Label("Check-In", systemImage: "calendar.badge.plus")
-                }
-                .tag(1)
-            
-            Text("Map View")
-                .tabItem {
-                    Label("Map", systemImage: "map.fill")
-                }
-                .tag(2)
-            
-            NotificationView()
-                .tabItem {
-                        Label("Notifications", systemImage: "bell.fill")
+            if (authViewModel.currentUser?.roles.contains(.patient) == true {
+                DashboardView()
+                    .tabItem {
+                        Label("Home", systemImage: "house.fill")
                     }
-                    .tag(3)
+                    .tag(0)
+                
+                CheckInView()
+                    .tabItem {
+                        Label("Check-In", systemImage: "calendar.badge.plus")
+                    }
+                    .tag(1)
+                
+                Text("Map View")
+                    .tabItem {
+                        Label("Map", systemImage: "map.fill")
+                    }
+                    .tag(2)
+                
+                NotificationView()
+                    .tabItem {
+                            Label("Notifications", systemImage: "bell.fill")
+                        }
+                        .tag(3)
+            }
+                if (authViewModel.currentUser?.roles.contains(.pharmacist) == true {
+                    DashboardView()
+                        .tabItem {
+                            Label("Home", systemImage: "house.fill")
+                        }
+                        .tag(0)
+                }
             
             ProfileView()
                 .tabItem {
